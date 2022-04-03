d034337e676c5854e08c9da5f16f9c941f04b88c
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-04-03 20:23:08 +0500

    Starting learning data Structues and Alogrithm. Day 1 Task is Pattern Question.

---
+++ Pattern Questions/FirstClass.java
@@ -0,0 +1,6 @@
+class FirstClass {
+    public static void main(String[] args) {
+      System.out.println("Day 01 : Starting Learning Data Structures and Algorithm");
+      System.out.println("Day 01 : Learn and solve multiple Pattern Questions");  
+    }
+}

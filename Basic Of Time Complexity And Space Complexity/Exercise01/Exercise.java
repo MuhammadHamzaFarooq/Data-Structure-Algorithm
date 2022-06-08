51464d0a596a4ce017dd3556ba4f88d05c6d81aa
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-06-08 15:52:06 +0500

    complete 5 pracitces question for java basic

--- Basic Of Time Complexity And Space Complexity/Exercise01/Exercise.java
+++ Basic Of Time Complexity And Space Complexity/Exercise01/Exercise.java
@@ -10,9 +10,26 @@ public class Exercise {
         System.out.println("Average => : " + result);
 
         // 2. Write a function to print the sum of all odd numbers from 1 to n
-        int oddSum =    math.sumOfAllOddNumbers(15);
-        System.out.println("Sum of Odd Numbers => : "+oddSum);
+        int oddSum = math.sumOfAllOddNumbers(15);
+        System.out.println("Sum of Odd Numbers => : " + oddSum);
+
+        // 3. Write a function which takes in 2 numbers and returns the greater of those
+        // two.
+        int greaterNo = math.greaterNumber(20, 40);
+        System.out.println("Greather no is => " + greaterNo);
+
+        // 4- Write a function that takes in the radius as input and returns the
+        // circumference of a circle
+        double circumference = math.circumferenceOfCircle(3.5);
+        System.out.println("Circumference Of Circle => " + circumference);
+
+        // 5 - Write a function that takes in age as input and returns if that person is eligible
+        // to vote or not. A person of age > 18 is eligible to vote.
+        math.checkVoteEligibility(18);
         
+
+
+
     }
 
 }
--- Basic Of Time Complexity And Space Complexity/Exercise01/maths/BasicMaths.java
+++ Basic Of Time Complexity And Space Complexity/Exercise01/maths/BasicMaths.java
@@ -1,7 +1,5 @@
 package maths;
 
-import org.w3c.dom.UserDataHandler;
-
 public class BasicMaths {
     public double average(int num1, int num2, int num3) {
         double avg = (num1 + num2 + num3) / 3;
@@ -18,5 +16,30 @@ public class BasicMaths {
             }
         }
         return sumOdd;
+    }
+
+    public int greaterNumber(int num1, int num2) {
+        int greaterNumber = 0;
+        if (num1 > num2) {
+            greaterNumber = num1;
+        } else if (num2 > num1) {
+            greaterNumber = num2;
+        } else if (num1 == num2) {
+            System.out.println("Equal Number");
+        }
+        return greaterNumber;
+    }
+
+    public double circumferenceOfCircle(double radius) {
+        double areaOfCircle = 2 * 2.142 * radius;
+        return areaOfCircle;
+    }
+
+    public void checkVoteEligibility(int age) {
+        if (age > 18) {
+            System.out.println(" person is eligible to vote");
+        } else {
+            System.out.println(" person is not eligible to vote");
+        }
     }
 }

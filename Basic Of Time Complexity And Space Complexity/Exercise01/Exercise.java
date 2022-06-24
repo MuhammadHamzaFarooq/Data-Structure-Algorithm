206e1c9fd1a0da0bc4b4f5737406474c63da3690
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-06-24 17:17:43 +0500

    all question done

--- Basic Of Time Complexity And Space Complexity/Exercise01/Exercise.java
+++ Basic Of Time Complexity And Space Complexity/Exercise01/Exercise.java
@@ -6,28 +6,54 @@ public class Exercise {
         // 1. Enter 3 numbers from the user & make a function to print their average.
         // Solution:
         maths.BasicMaths math = new maths.BasicMaths();
-        double result = math.average(20, 30, 48);
-        System.out.println("Average => : " + result);
+         double result = math.average(20, 30, 48);
+         System.out.println("Average => : " + result);
 
         // 2. Write a function to print the sum of all odd numbers from 1 to n
-        int oddSum = math.sumOfAllOddNumbers(15);
-        System.out.println("Sum of Odd Numbers => : " + oddSum);
+         int oddSum = math.sumOfAllOddNumbers(15);
+         System.out.println("Sum of Odd Numbers => : " + oddSum);
 
         // 3. Write a function which takes in 2 numbers and returns the greater of those
         // two.
-        int greaterNo = math.greaterNumber(20, 40);
-        System.out.println("Greather no is => " + greaterNo);
+         int greaterNo = math.greaterNumber(20, 40);
+         System.out.println("Greather no is => " + greaterNo);
 
         // 4- Write a function that takes in the radius as input and returns the
         // circumference of a circle
-        double circumference = math.circumferenceOfCircle(3.5);
-        System.out.println("Circumference Of Circle => " + circumference);
+         double circumference = math.circumferenceOfCircle(3.5);
+         System.out.println("Circumference Of Circle => " + circumference);
 
-        // 5 - Write a function that takes in age as input and returns if that person is eligible
+        // 5 - Write a function that takes in age as input and returns if that person is
+        // eligible
         // to vote or not. A person of age > 18 is eligible to vote.
-        math.checkVoteEligibility(18);
-        
+         math.checkVoteEligibility(18);
 
+        // 6- Write an infinite loop using do while condition.
+
+         math.infiniteLoop();
+
+        // 7-Write a program to enter the numbers till the user wants and at the end it
+        // should display the count of positive, negative and zeros entered.
+
+         math.displaySummary();
+
+        // 8 - Two numbers are entered by the user, x and n. Write a function to find
+        // the value of one number raised to the power of another i.e. 𝑥 .�
+
+        math.power();
+
+        // 9 - Write a function that calculates the Greatest Common Divisor of 2
+        // numbers.
+
+         math.greatestCommonDivisor();
+
+        // 10 - Write a program to print Fibonacci series of n terms where n is input
+        // by user :
+        // 0 1 1 2 3 5 8 13 21 .....
+        // In the Fibonacci series, a number is the sum of the previous 2 numbers that
+        // came before it.
+
+        math.fibonacciSequence();
 
 
     }
--- Basic Of Time Complexity And Space Complexity/Exercise01/maths/BasicMaths.java
+++ Basic Of Time Complexity And Space Complexity/Exercise01/maths/BasicMaths.java
@@ -1,5 +1,12 @@
 package maths;
 
+import java.util.*;
+
+import javax.lang.model.util.ElementScanner14;
+import javax.naming.NamingException;
+
+import java.math.*;;
+
 public class BasicMaths {
     public double average(int num1, int num2, int num3) {
         double avg = (num1 + num2 + num3) / 3;
@@ -42,4 +49,117 @@ public class BasicMaths {
             System.out.println(" person is not eligible to vote");
         }
     }
+
+    public void infiniteLoop() {
+        int start = 0;
+        do {
+            System.out.println("Infinite Loop!!!");
+            start++;
+        } while (start > 0);
+    }
+
+    public void displaySummary() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your Total Number Length");
+        int size = sc.nextInt();
+
+        System.out.print("Enter the number of elements you want to store: ");
+        int[] numbers = new int[size];
+
+        // Assigning Value in Array
+        for (int j = 0; j < size; j++) {
+            numbers[j] = sc.nextInt();
+        }
+
+        int positiveCount = 0;
+        int negativeCount = 0;
+        int zerosCount = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] >= 1) {
+                positiveCount += 1;
+            } else if (numbers[i] < 0) {
+                negativeCount += 1;
+            } else if (numbers[i] == 0) {
+                zerosCount += 1;
+            }
+        }
+
+        System.out.println("Positive Count " + positiveCount);
+        System.out.println("Negative Count " + negativeCount);
+        System.out.println("Zero Count " + zerosCount);
+    }
+
+    public void power() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Base Number");
+        int x = sc.nextInt();
+
+        System.out.println("Enter Power Number");
+        int n = sc.nextInt();
+
+        double power = Math.pow(x, n);
+
+        System.out.println("X ^ n is => " + power);
+    }
+
+    public void greatestCommonDivisor() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number1");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter Number2");
+        int num2 = sc.nextInt();
+
+        while(num1 != num2){
+             if(num1>num2){
+                num1 = num1 - num2;
+             }
+             else{
+                num2 = num2 - num1;
+             }
+        }
+        // int firstNumberDivisors[] = new int[20];
+        // int secondtNumberDivisors[] = new int[20];
+        // int common[] = new int[20];
+
+        // for (int i = 1; i <= 20; i++) {
+        //     if (i % num1 == 0) {
+        //         firstNumberDivisors[i] += i;
+        //     }
+        // }
+
+        // for (int i = 1; i <= 20; i++) {
+        //     if (i % num2 == 0) {
+        //         secondtNumberDivisors[i] += i;
+        //     }
+        // }
+
+        // for (int j = 0; j < firstNumberDivisors.length; j++) {
+        //     if (firstNumberDivisors[j] == secondtNumberDivisors[j]) {
+        //         common[j] += firstNumberDivisors[j];
+        //     }
+
+        // }
+
+        System.out.println("Common => "+num2);
+    }
+
+    // FibonacciSeries => 0  1  1  2  
+    public void fibonacciSequence(){
+        int first = 0;
+        int second = 1 ;
+        int third ;
+        System.out.print(first + " "+second);
+        for(int i = 1 ; i <= 10 ; i++ ){   
+            third = first + second;
+            System.out.print(" "+ third + " ");
+            first = second;
+            second = third;
+        }
+
+    
+    }
+
+
 }
